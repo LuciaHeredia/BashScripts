b2b389bac43bfa0adbe5b38a01060b8c8c4841a5
Lucia Heredia <luciaheredia22@gmail.com>
2024-12-15 16:56:07 +0200

    check_even_odd.sh done

--- check_even_odd.sh
+++ check_even_odd.sh
@@ -2,8 +2,8 @@
 
 read -p "Enter a number: " num
 
-if [ (($num % 2)) -eq 0]; then
-	eho "The number is even."
+if [ $(($num % 2)) -eq 0 ]; then
+	echo "The number is even."
 else
 	echo "The number is odd."
 fi
