36194ddf7f3c2cd11c056b974b4bc54a4033de8b
Lucia Heredia <luciaheredia22@gmail.com>
2024-12-25 22:55:56 +0200

    done

--- grep/recursive-directory-search.sh
+++ grep/recursive-directory-search.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 
+###### Start msg
+echo -e "Recursive directory search, for TODO tasks: \n"
+
+###### Directory path
+read -p "Enter directory path: " path
+# check directory exists
+if [ ! -d "$path" ]; then
+	echo "Directory path doesn't exist!"
+	exit	
+fi
+
+###### Search
+echo -e "\nResults:"
+#-r :recursively inside directory
+#-n :line number
+matches=$(grep -rn "TODO" "$path") 
+if [ "$matches" ]; then
+	echo "$matches"
+	#-l :count lines
+	echo "Total matches: $( echo "$matches" | wc -l)"
+else
+	echo "None"
+fi
+
